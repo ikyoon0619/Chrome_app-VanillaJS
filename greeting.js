44472d1647de852eb5fc47ef0cb3dc2f530cd00d
allegory_one <ikyoon0619@gmail.com>
2021-05-25 18:20:25 +0900

    유저 네임 저장 기능 추가

---
+++ greeting.js
@@ -0,0 +1,36 @@
+const formEl = document.querySelector(".js-form");
+const inputEl = formEl.querySelector("input");
+const greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser";
+
+function paintGreeting(text) {
+  formEl.classList.remove("showing");
+  greeting.classList.add("showing");
+  greeting.textContent = `Hello ${text}`;
+}
+
+function setName() {
+  formEl.classList.add("showing");
+  formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const currentValue = inputEl.value;
+    paintGreeting(currentValue);
+    localStorage.setItem(USER_LS, currentValue);
+  });
+}
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    // 사용자가 없다면
+    setName();
+  } else {
+    paintGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
