e4a6ea5bfd37dfc3118b392461d5f6de21046cd9
allegory_one <ikyoon0619@gmail.com>
2021-05-31 15:48:57 +0900

    weather.js: fetch api

--- weather.js
+++ weather.js
@@ -1,4 +1,18 @@
+const API_KEY = "23841a223f142e8c4b26b7a36f50d03a";
 const COORDS = "coords";
+const weather = document.querySelector(".js-weather");
+
+function getWeather(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const temperature = data.main.temp;
+      const place = data.name;
+      weather.textContent = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +26,7 @@ function handleGeoSuccess(position) {
     longitude,
   };
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -28,6 +43,8 @@ function loadCoords() {
     askForCoords();
   } else {
     // getWeather
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
   }
 }
 
