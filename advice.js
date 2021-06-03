ed0ddd3cfefe55f7890c7f8bf83a7a069d828a40
allegory_one <ikyoon0619@gmail.com>
2021-06-03 12:30:45 +0900

    advice.js: 명언 앞쪽과 뒤에 큰따옴표 넣기

--- advice.js
+++ advice.js
@@ -1,22 +1,17 @@
-
 const ADVICE_URL = "https://api.adviceslip.com/advice";
 
 function fetchAdvice() {
- return fetch(ADVICE_URL)
- .then((response) => response.json())
- .then(data=>{
-  const pEl = document.createElement("p");
-  const divEl = document.querySelector(".advice");
-  pEl.textContent = data.slip.advice;
-  divEl.appendChild(pEl);
- })
-    
-  }
+  return fetch(ADVICE_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      const pEl = document.createElement("p");
+      const divEl = document.querySelector(".advice");
+      pEl.textContent = `" ${data.slip.advice}"`;
+      divEl.appendChild(pEl);
+    });
+}
 
-
-
-function init(){
-
+function init() {
   fetchAdvice();
 }
 
